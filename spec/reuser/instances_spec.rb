3d96a0d77651a98c6883359dc8014dbe02c181be
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-01 20:34:40 -0500

    Refactored the specs

--- spec/reuser/instances_spec.rb
+++ spec/reuser/instances_spec.rb
@@ -1,10 +1,16 @@
-require_relative 'spec_helper'
+require_relative '../spec_helper'
 
 describe "Instances of a Class including ReUser" do
   let(:klass) do
     kls = Class.new
     kls.instance_eval do
       include ReUser
+
+      roles do
+        role :admin do |admin|
+          admin.can :read
+        end
+      end
     end
     kls
   end
@@ -22,23 +28,11 @@ describe "Instances of a Class including ReUser" do
     end
 
     it "if the role has the permission defined, returns true" do
-      klass.roles do
-        klass.role :admin do |admin|
-          admin.can :read
-        end
-      end
-
       subject.can?(:read).should be_true
     end
 
     it "if the role does not define the permission, returns false" do
-      klass.roles do
-        klass.role :admin do |admin|
-          admin.can :read
-        end
-      end
-
-      subject.can?(:read).should be_true
+      subject.can?(:write).should be_false
     end
   end
 end
